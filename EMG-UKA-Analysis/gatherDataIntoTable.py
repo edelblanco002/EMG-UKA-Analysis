4aab0ff426459f2ba59924017f64be0c2fb8b031
edelblanco002 <ederdelblanco@gmail.com>
2021-10-21 12:44:09 +0200

    gatherDataIntoTable module changed and documented
    The option off building a table just with the utterances of an specific
    speaker and session has been added.
    
    Also, the module has been documented.

--- EMG-UKA-Analysis/gatherDataIntoTable.py
+++ EMG-UKA-Analysis/gatherDataIntoTable.py
@@ -3,11 +3,11 @@ import numpy as np
 import os
 import tables
 
-def buildTable(dirpath,utteranceFiles,rowSize,tableFileName='table'):
+def buildTable(dirPath,utteranceFiles,rowSize,tableFileName='table'):
     # This function reads the data from all files in the utteranceFiles list
     # and writes all together into a HDF5 file
 
-    tableFile = tables.open_file(f"{dirpath}/{tableFileName}.h5",mode='w', title="Features from audible utterances")
+    tableFile = tables.open_file(f"{dirPath}/{tableFileName}.h5",mode='w', title="Features from audible utterances")
     atom = tables.Float32Atom()
     
     # Create the array_c to write into the HDF5 file. Each example is appended as a new row
@@ -19,7 +19,7 @@ def buildTable(dirpath,utteranceFiles,rowSize,tableFileName='table'):
         utteranceFile = utteranceFile.replace('emg_','')
         speaker, session, utt = utteranceFile.split('-')
         
-        file = open(f"{dirpath}/features/{speaker}/{session}/e07_{speaker}_{session}_{utt}.npy",'rb')
+        file = open(f"{dirPath}/features/{speaker}/{session}/e07_{speaker}_{session}_{utt}.npy",'rb')
         auxMat = np.load(file)
         file.close()
     
@@ -35,8 +35,8 @@ def buildTable(dirpath,utteranceFiles,rowSize,tableFileName='table'):
     return
 
 
-def getFilesList(dirpath,uttType,subset='both'):
-    # This function returns the list of all utterances of the given uttType (audible, whispered or silent) and the given subset (train, test or both)
+def getFilesList(dirPath,uttType,subset='both',speaker='all',session='all'):
+    # This function returns the list of all utterances of the given uttType (audible, whispered or silent) and the given subset (train, test or both). Also a specific speaker and session can be selected.
 
     utteranceFiles = []
 
@@ -48,20 +48,35 @@ def getFilesList(dirpath,uttType,subset='both'):
         utteranceListFiles = [f"test.{uttType}"]
 
     for listFile in utteranceListFiles:
-        file = open(f"{dirpath}/Subsets/{listFile}",'r')
+        file = open(f"{dirPath}/Subsets/{listFile}",'r')
         lines = file.readlines()
         file.close()
-    
-        for line in lines:
-            if line.strip():
-                line = line.replace('\n','')
-                for element in line.split(' ')[1:]:
-                    if element != '':
-                        utteranceFiles.append(element)
+
+        if speaker == 'all': # Extract files from every session of every users
+            for line in lines:
+                if line.strip():
+                    line = line.replace('\n','')
+                    for element in line.split(' ')[1:]:
+                        if element != '':
+                            utteranceFiles.append(element)
+
+        else: # Extract the files from an specific user
+            for line in lines:
+                if line.strip():
+                    line = line.replace('\n','')
+                    id = line.split(':')[0]
+                    id = id.replace('emg_','')
+                    idSpeaker, idSession = id.split('-')
+                    if speaker == idSpeaker and (session == 'all' or session == idSession):
+                    # Only enters here if the line corresponds to the desired user. Also, only enters here if every session of the user are wanted or, otherwise, the line corresponds to the desired session
+                        for element in line.split(' ')[1:]:
+                            if element != '':
+                                utteranceFiles.append(element)
+
     return utteranceFiles
 
 
-def main(dirpath='C:/Users/Eder/Downloads/EMG-UKA-Trial-Corpus',uttType = 'audible',subset='both'):
+def main(dirPath,uttType,subset='both',speaker='all',session='all'):
     
     nChannels = 6
     nFeatures = 5
@@ -69,14 +84,24 @@ def main(dirpath='C:/Users/Eder/Downloads/EMG-UKA-Trial-Corpus',uttType = 'audib
 
     rowSize = 6*5*(stackingWidth*2+1) + 1
     
-    files = getFilesList(dirpath,uttType,subset)
+    files = getFilesList(dirPath,uttType,subset)
 
-    if subset == 'both':
-        filename = f"{uttType}Table"
-    else:
-        filename = f"{uttType}_{subset}_Table"
+    filename = ""
 
-    buildTable(dirpath,files,rowSize,filename)
+    if speaker != 'all':
+        filename += f"{speaker}_"
+
+    if session != 'all':
+        filename += f"{session}_"
+
+    filename += f"{uttType}"
+
+    if subset != 'both':
+        filename += f"_{subset}_"
+
+    filename += 'Table'
+
+    buildTable(dirPath,files,rowSize,filename)
 
 
 if __name__ == '__main__':
